030630b8c2b5071d7eb3118810de51e21d0841bf
GirishPulinkala <98015303+GirishPulinkala@users.noreply.github.com>
2025-02-17 14:17:03 +0100

    Update visuArc.R

--- visuArc.R
+++ visuArc.R
@@ -13,7 +13,7 @@ visuArc <- function(net, decision, feature=NULL){
   
   #make df:  a data frame containing network connection in columns
  # df<- eval(parse(text=paste("net$", decision,'$nodes' ,sep = "")))
-  df<-net[[decision]][[nodes]]
+  df<-net[[decision]][['nodes']]
   cols_select<- c('label','DiscState','NodeConnection')
   df<- subset(df,select = cols_select)
   rownames(df)  <-make.names(df[,'label'], unique = TRUE)
